4fd5164f3c5a7c5778fe87a4dec2e2748aa0b8bf
qWest <vladimir.gainullin@gmail.com>
2020-06-04 23:44:33 +0300

    1.6

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,27 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+extra_payment = 1000
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+
+
+
+while principal > 0:
+    month += 1
+    if month <= extra_payment_end_month and month >= extra_payment_start_month:
+        principal = principal * (1 + rate / 12) - payment - extra_payment
+        total_paid = total_paid + payment + extra_payment
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+    print(month, round(total_paid, 2), round(principal, 2))
+
+print ('Total paid', round(total_paid, 2))
+print ('Month', month)
